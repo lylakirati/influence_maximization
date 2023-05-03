e22e4341b5fdf01c6a0839d8442a0537a0368136
LylaK <lylakirati@gmail.com>
2023-05-03 11:41:36 -0400

    fixed bugs in IC, added docstrings, example file

--- models/independentCascade.py
+++ models/independentCascade.py
@@ -2,28 +2,62 @@ import networkx as nx
 import numpy as np
 
 class IndependentCascade():
+    """(Weighted) Independent Cascade Model of Social Contagion
     
-    def __init__(self, graph, seeds, weighted = True):
+    Attributes
+    ------
+    graph : networkx.classes.digraph.DiGraph object
+        full network graph that information will spread on
+    seeds : list of int
+        list of initial seeds
+    activated : set of int
+        set of activated nodes
+    inactivated : set of int
+        set of inactivated nodes
+        
+    Examples
+    ------
+    >>> ic_model = IndependentCascade(graph, seeds)
+    >>> activated = ic_model.simulate(weighted = False, prob = 0.2)
+    >>> print(ic_model.get_spread())
+    135
+    >>> avg_spread = ic_model.run_simulations(weighted = True, iters = 10)
+    >>> print(f"Average spread across 10 runs: {avg_spread}")
+    Average spread across 10 runs: 97.6
+    """
+    
+    def __init__(self, graph, seeds):
         self.graph = graph
         self.seeds = seeds
-        self.weighted = weighted
         self.activated = set()
         self.inactivated = set(self.graph.nodes)
         
-    def __call__(self, *args, **kwargs):
-        if self.weighted:
-            return self.simulate_weighted_IC(*args, **kwargs)
+    def simulate(self, weighted = True, prob = None):
+        if (prob is None) and (not weighted):
+            raise ValueError("Must specify activation probability for non-weighted IC simulation")
+        
+        if weighted:
+            return self.simulate_weighted_IC()
         else:
-            return self.simulate_IC(*args, **kwargs)
+            return self.simulate_IC(prob)
+    
+    
+    def run_simulations(self, weighted = True, prob = None, iters = 5):
+        spreads = [len(self.simulate(weighted, prob)) for i in range(iters)]
+        return np.mean(spreads)
+    
         
     def get_activated(self):
         return self.activated
     
+    
     def get_inactivated(self):
         return self.inactivated
     
+    
     def get_spread(self):
         return len(self.activated)
+    
         
     def simulate_IC(self, prob = 0.2):
         if prob <= 0 or prob > 1:
@@ -33,7 +67,7 @@ class IndependentCascade():
         self.inactivated -= self.activated
         spreading = self.seeds
         
-        while len(self.inactivated) != 0:
+        while len(self.inactivated) <= 0 or len(spreading) > 0:
             spreading_next = []
             for node in spreading:
                 neighbors = list(set(self.graph.neighbors(node)) - set(self.activated))
@@ -46,14 +80,14 @@ class IndependentCascade():
                 break
         
         return self.activated
-        
+
     
     def simulate_weighted_IC(self):
         self.activated = set(self.seeds)
-        self.inactivated -= self.activated
+        self.inactivated = set(self.graph.nodes) - self.activated
         spreading = self.seeds
         
-        while len(self.inactivated) != 0:
+        while len(self.inactivated) <= 0 or len(spreading) > 0:
             spreading_next = []
             for node in spreading:
                 neighbors = list(set(self.graph.neighbors(node)) - set(self.activated))
@@ -64,8 +98,6 @@ class IndependentCascade():
                 self.inactivated -= set(infected)
                 spreading_next = spreading_next + infected
             spreading = spreading_next
-            if len(spreading) == 0:
-                break
-        
+    
         return self.activated
         
